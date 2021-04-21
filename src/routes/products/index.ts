367947a71665737b972f7a7f3f1758f1e074e9ca
rushabha-jain <rushabha.jain@outlook.com>
2021-04-21 12:53:05 +0530

    Added support for database access to the list products api

--- src/routes/products/index.ts
+++ src/routes/products/index.ts
@@ -1,19 +1,17 @@
 import express from "express";
-
+import { ProductStore } from "../../models/ProductStore";
 const productRouter = express.Router();
 
-productRouter.get("/", (req, res) => {
-  res.status(200).send({
-    status: "success",
-    data: [
-      {
-        name: "Rushabha"
-      },
-      {
-        name: "Arihant"
-      }
-    ]
-  });
+productRouter.get("/", async (req, res) => {
+  try {
+    const products = await ProductStore.index();
+    res.status(200).send({
+      status: "success",
+      data: products
+    });
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 export default productRouter;
