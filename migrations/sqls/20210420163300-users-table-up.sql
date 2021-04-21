30bd2e1091f98ad6627868f3e17014ff1897253e
rushabha-jain <rushabha.jain@outlook.com>
2021-04-21 16:58:46 +0530

    Added email field

--- migrations/sqls/20210420163300-users-table-up.sql
+++ migrations/sqls/20210420163300-users-table-up.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS users(
     id BIGSERIAL PRIMARY KEY,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
