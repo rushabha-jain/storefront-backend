e12767a9ef56ceaff1a36e910c1fe99270d991cc
rushabha-jain <rushabha.jain@outlook.com>
2021-04-21 16:58:29 +0530

    Added create and show methods

--- src/models/ProductStore.ts
+++ src/models/ProductStore.ts
@@ -7,14 +7,47 @@ export interface Product {
 }
 
 export class ProductStore {
-  static async index(): Promise<Product[]> {
+  async index(): Promise<Product[]> {
     try {
       const databaseConnection = await Client.connect();
-      const productTable = await databaseConnection.query("SELECT * FROM products");
+      const productTable = await databaseConnection.query(
+        "SELECT * FROM products"
+      );
       databaseConnection.release();
       return productTable.rows;
     } catch (error) {
       throw new Error(`Unable to get products ${error}`);
+    }
+  }
+
+  async create(product: Product): Promise<Product> {
+    try {
+      const databaseConnection = await Client.connect();
+      const productTable = await databaseConnection.query(
+        "INSERT INTO products(name, price) VALUES($1, $2)",
+        [product.name, product.price]
+      );
+      databaseConnection.release();
+      return productTable.rows[0];
+    } catch (error) {
+      throw new Error(`Unable to create product ${error}`);
+    }
+  }
+
+  async show(id: string): Promise<Product | null> {
+    try {
+      const databaseConnection = await Client.connect();
+      const productTable = await databaseConnection.query(
+        "SELECT * FROM products WHERE id=$1",
+        [id]
+      );
+      databaseConnection.release();
+      if (productTable.rowCount > 0) {
+        return productTable.rows[0];
+      }
+      return null;
+    } catch (error) {
+      throw new Error(`Unable to get product ${error}`);
     }
   }
 }
