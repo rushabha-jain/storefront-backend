c35a3ebc920c57eb30014376f45fc65515b43345
rushabha-jain <rushabha.jain@outlook.com>
2021-04-22 13:25:00 +0530

    Updated datatype of status

--- migrations/sqls/20210420163530-orders-table-up.sql
+++ migrations/sqls/20210420163530-orders-table-up.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS orders(
     user_id BIGINT REFERENCES users(id),
     product_id BIGINT REFERENCES products(id),
     quantity INT NOT NULL,
-    status VARCHAR(50) NOT NULL DEFAULT 'active'
+    status INT NOT NULL DEFAULT 0
 );
