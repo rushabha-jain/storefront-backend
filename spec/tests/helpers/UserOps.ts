ed3b51bab48afbefb1251bc9675ab943adf160d3
rushabha-jain <rushabha.jain@outlook.com>
2021-04-23 23:50:52 +0530

    Exported AuthUser type containing token property

--- spec/tests/helpers/UserOps.ts
+++ spec/tests/helpers/UserOps.ts
@@ -1,13 +1,13 @@
 import { UserStore, User } from "../../../src/models/UserStore";
 import { generateJWTToken } from "../../../src/util/jwtToken";
 
+export type AuthUser = User & {
+  token: string;
+};
+
 const userStore = new UserStore();
 
-export async function createRandomUser(): Promise<
-  User & {
-    token: string;
-  }
-> {
+export async function createRandomUser(): Promise<AuthUser> {
   const user = {
     firstname: "random",
     lastname: "random",
