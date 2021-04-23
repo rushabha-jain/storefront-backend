9c0bbb708b9aeeb477c76bf31457fe5866fc55f7
rushabha-jain <rushabha.jain@outlook.com>
2021-04-23 23:50:30 +0530

    Changed type of test order

--- spec/tests/helpers/OrderOps.ts
+++ spec/tests/helpers/OrderOps.ts
@@ -1,12 +1,19 @@
 import { OrderStore, Order } from "../../../src/models/OrderStore";
-import { createRandomUser } from "./UserOps";
+import { createRandomUser, AuthUser } from "./UserOps";
 
 const orderStore = new OrderStore();
-export async function createRandomOrder(): Promise<Order> {
+export async function createRandomOrder(): Promise<
+  Order & {
+    user: AuthUser;
+  }
+> {
   const randomUser = await createRandomUser();
   const order: Order = {
     user_id: randomUser.id as number
   };
   const newOrder = await orderStore.create(order);
-  return newOrder;
+  return {
+    ...newOrder,
+    user: randomUser
+  };
 }
