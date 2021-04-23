7a99ef9f1662e13701de6155d188f1f40d25787a
rushabha-jain <rushabha.jain@outlook.com>
2021-04-23 23:33:42 +0530

    Refactored code

--- spec/tests/models/OrderProductSpec.ts
+++ spec/tests/models/OrderProductSpec.ts
@@ -2,11 +2,9 @@ import {
   OrderProductStore,
   OrderProduct
 } from "../../../src/models/OrderProductStore";
-import {
-  createRandomProduct,
-  deleteRandomProduct
-} from "../helpers/ProductOps";
-import { createRandomOrder, deleteRandomOrder } from "../helpers/OrderOps";
+import { createRandomProduct } from "../helpers/ProductOps";
+import { createRandomOrder } from "../helpers/OrderOps";
+import cleanup from "../helpers/cleanup";
 
 const orderProductStore = new OrderProductStore();
 let productId: number, orderId: number;
@@ -20,10 +18,8 @@ describe("OrderProduct Model", () => {
     orderId = newOrder.id as number;
   });
   afterAll(async () => {
-    await deleteRandomOrder(orderId);
-    await deleteRandomProduct(productId);
+    await cleanup();
   });
-
   it("Should add product in the order", async () => {
     const orderProduct: OrderProduct = {
       product_id: productId,
@@ -31,7 +27,6 @@ describe("OrderProduct Model", () => {
       quantity: 10
     };
     const newProductOrder = await orderProductStore.create(orderProduct);
-    orderId = newProductOrder.id as number;
     expect(newProductOrder.id).toBeDefined();
   });
 
